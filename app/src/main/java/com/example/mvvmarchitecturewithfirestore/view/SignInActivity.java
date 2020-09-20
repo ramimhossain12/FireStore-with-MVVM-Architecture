e2f7203237f3c4a56bea45b11f99eb10a1867ee6
ramimhossain12 <ramimhossain6242@gmail.com>
2020-09-20 22:21:52 +0600

    Google signIn

--- app/src/main/java/com/example/mvvmarchitecturewithfirestore/view/SignInActivity.java
+++ app/src/main/java/com/example/mvvmarchitecturewithfirestore/view/SignInActivity.java
@@ -1,19 +1,31 @@
 package com.example.mvvmarchitecturewithfirestore.view;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.mvvmarchitecturewithfirestore.R;
 import com.example.mvvmarchitecturewithfirestore.viewmodel.SignInViewModel;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 public class SignInActivity extends AppCompatActivity {
 
+    private static final int RC_SIGN_IN = 1;
     private Button button;
+    private GoogleSignInClient mGoogleSignInClient;
 
     private SignInViewModel signInViewModel;
 
@@ -24,6 +36,7 @@ public class SignInActivity extends AppCompatActivity {
 
 
         intiSignInViewModel();
+        signInMethod();
 
         button = findViewById(R.id.signInButtonID);
 
@@ -31,19 +44,72 @@ public class SignInActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-              signIn();
+                signIn();
             }
         });
     }
 
+    private void signInMethod() {
+
+        // Configure Google Sign In
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+
+        // Build a GoogleSignInClient with the options specified by gso.
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+    }
+
     private void intiSignInViewModel() {
 
-        signInViewModel = new ViewModelProvider(this,ViewModelProvider.AndroidViewModelFactory.getInstance(this.getApplication()))
+        signInViewModel = new ViewModelProvider(this, ViewModelProvider.AndroidViewModelFactory.getInstance(this.getApplication()))
                 .get(SignInViewModel.class);
     }
 
     private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                if (account != null) {
 
 
+                    getGoogleAuthCreadential(account);
+                }
+            } catch (ApiException e) {
+                Toast.makeText(this,e.toString(),Toast.LENGTH_SHORT).show();
+
+
+            }
+        }
+    }
+
+    private void getGoogleAuthCreadential(GoogleSignInAccount account) {
+
+        String  googleTokenID = account.getIdToken();
+        AuthCredential authCredential = GoogleAuthProvider.getCredential(googleTokenID,null);
+        signInViewModel.signInWithGoogle(authCredential);
+        signInViewModel.auticationLiveData.observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(String s) {
+                Toast.makeText(SignInActivity.this,""+s,Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(SignInActivity.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
