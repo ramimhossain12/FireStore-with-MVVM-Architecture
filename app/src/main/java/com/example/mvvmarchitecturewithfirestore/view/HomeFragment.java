51e0e9fe2e9ded31796ae017e158857fe80a000e
ramimhossain12 <ramimhossain6242@gmail.com>
2020-09-22 12:09:02 +0600

    Home Fragment

--- app/src/main/java/com/example/mvvmarchitecturewithfirestore/view/HomeFragment.java
+++ app/src/main/java/com/example/mvvmarchitecturewithfirestore/view/HomeFragment.java
@@ -2,18 +2,26 @@ package com.example.mvvmarchitecturewithfirestore.view;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.mvvmarchitecturewithfirestore.R;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 
 public class HomeFragment extends Fragment {
 
-
+    private Button signOutButton;
+    private CircleImageView profileImageView;
+    private TextView nameTextView,emailTextView;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -25,5 +33,16 @@ public class HomeFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+        signOutButton = view.findViewById(R.id.signOutButtonId);
+        profileImageView= view.findViewById(R.id.profileImageId);
+        nameTextView= view.findViewById(R.id.profileNameId);
+        emailTextView= view.findViewById(R.id.profileEmailId);
     }
 }
