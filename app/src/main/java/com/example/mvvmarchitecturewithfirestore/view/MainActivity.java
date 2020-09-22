33bff82ef222e3b6ecee36c082bdf752638ebdd5
ramimhossain12 <ramimhossain6242@gmail.com>
2020-09-22 11:53:40 +0600

    Fragment

--- app/src/main/java/com/example/mvvmarchitecturewithfirestore/view/MainActivity.java
+++ app/src/main/java/com/example/mvvmarchitecturewithfirestore/view/MainActivity.java
@@ -1,16 +1,27 @@
 package com.example.mvvmarchitecturewithfirestore.view;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
 import com.example.mvvmarchitecturewithfirestore.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
+    private NavController navController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationId);
+        navController = Navigation.findNavController(this,R.id.nav_host_fragment);
+        NavigationUI.setupWithNavController(bottomNavigationView,navController);
     }
 }
