c393ccb9adbe2bd5509046740aa3da763950c487
ramimhossain12 <ramimhossain6242@gmail.com>
2020-09-22 22:39:57 +0600

    Imgage intent and Crop image

--- app/src/main/java/com/example/mvvmarchitecturewithfirestore/view/InsertFragment.java
+++ app/src/main/java/com/example/mvvmarchitecturewithfirestore/view/InsertFragment.java
@@ -1,22 +1,43 @@
 package com.example.mvvmarchitecturewithfirestore.view;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.mvvmarchitecturewithfirestore.R;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class InsertFragment extends Fragment {
 
+    private CircleImageView insertImageView;
+    private EditText insertNameEditText;
+    private EditText insertPhoneEditText;
+    private Button saveButton;
+    private EditText insertEmailEditText;
+    private Uri insertImageUri;
 
     public InsertFragment() {
         // Required empty public constructor
     }
-
 
 
     @Override
@@ -24,5 +45,58 @@ public class InsertFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_insert, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        insertImageView = view.findViewById(R.id.insertImageId);
+        insertNameEditText = view.findViewById(R.id.insertNameId);
+        insertPhoneEditText = view.findViewById(R.id.insertPhoneId);
+        insertEmailEditText = view.findViewById(R.id.insertEmailId);
+        saveButton = view.findViewById(R.id.saveButtonId);
+        insertImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                uploadImage();
+            }
+        });
+    }
+
+    private void uploadImage() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+            } else {
+                imagePick();
+            }
+        } else {
+            imagePick();
+        }
+
+
+    }
+
+    private void imagePick() {
+        CropImage.activity().setGuidelines(CropImageView.Guidelines.ON)
+                .setAspectRatio(1, 1)
+                .start(getContext(), this);
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            if (resultCode == getActivity().RESULT_OK) {
+                insertImageUri = result.getUri();
+                insertImageView.setImageURI(insertImageUri);
+            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+                Exception error = result.getError();
+            }
+        }
     }
 }
